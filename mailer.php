80621009ec74c000e82c8a48cbbf483127346cf5
saraweinand <saraweinand@gmail.com>
2015-04-03 10:19:01 -0700

    emailtest

--- mailer.php
+++ mailer.php
@@ -1,62 +1,77 @@
 <?php
 /* Set e-mail recipient */
-$myemail = "saraweinand@gmail.com";
+$myemail  = "saraweinand@gmail.com";
 
 /* Check all form inputs using check_input function */
-$name = check_input($_POST['name'], "Enter your name");
-$subject = check_input($_POST['subject'], "Enter a subject");
-$email = check_input($_POST['email']);
-$message = check_input($_POST['message'], "Write your message");
+$yourname = check_input($_POST['yourname'], "Enter your name");
+$subject  = check_input($_POST['subject'], "Write a subject");
+$email    = check_input($_POST['email']);
+$website  = check_input($_POST['website']);
+$likeit   = check_input($_POST['likeit']);
+$how_find = check_input($_POST['how']);
+$comments = check_input($_POST['comments'], "Write your comments");
 
 /* If e-mail is not valid show error message */
 if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
 {
-show_error("E-mail address not valid");
+    show_error("E-mail address not valid");
 }
+
+/* If URL is not valid set $website to empty */
+if (!preg_match("/^(https?:\/\/+[\w\-]+\.[\w\-]+)/i", $website))
+{
+    $website = '';
+}
+
 /* Let's prepare the message for the e-mail */
-$message = "
+$message = "Hello!
 
-Name: $name
+Your contact form has been submitted by:
+
+Name: $yourname
 E-mail: $email
-Subject: $subject
+URL: $website
 
-Message:
-$message
+Like the website? $likeit
+How did he/she find it? $how_find
 
+Comments:
+$comments
+
+End of message
 ";
 
 /* Send the message using mail() function */
 mail($myemail, $subject, $message);
 
 /* Redirect visitor to the thank you page */
-header('Location: thanks.html');
+header('Location: thanks.htm');
 exit();
 
 /* Functions we used */
 function check_input($data, $problem='')
 {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-if ($problem && strlen($data) == 0)
-{
-show_error($problem);
-}
-return $data;
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if ($problem && strlen($data) == 0)
+    {
+        show_error($problem);
+    }
+    return $data;
 }
 
 function show_error($myError)
 {
 ?>
-<html>
-<body>
+    <html>
+    <body>
 
-<p>Please correct the following error:</p>
-<strong><?php echo $myError; ?></strong>
-<p>Hit the back button and try again</p>
+    <b>Please correct the following error:</b><br />
+    <?php echo $myError; ?>
 
-</body>
-</html>
+    </body>
+    </html>
 <?php
 exit();
 }
