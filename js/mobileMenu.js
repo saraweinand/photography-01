a2d974788093a84ae08e368ac9d632a6515ae7e4
saraweinand <saraweinand@gmail.com>
2015-05-19 20:52:50 -0700

    responsive

---
+++ js/mobileMenu.js
@@ -0,0 +1,16 @@
+$(function() {
+  $("a[href=#menuExpand]").click(function(e) {
+  showHideMobile();
+  e.preventDefault();
+  });
+  
+  $(".mobileDimmer").click(function() {
+    showHideMobile();
+  });
+});
+
+function showHideMobile() {
+  $(".mobileMenu").toggleClass("menuOpen");
+  $(".mobileBodyWrapper").toggleClass("menuOpen");
+  $(".mobileDimmer").toggle();
+}
